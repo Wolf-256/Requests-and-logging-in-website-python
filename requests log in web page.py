46373509e2c42526fe93613af428489943b406af
Wolf-256 <93338953+Wolf-256@users.noreply.github.com>
2022-05-15 11:06:25 -0400

    Add files via upload
    Python code requests

---
+++ requests log in web page.py
@@ -0,0 +1,12 @@
+#requests needed to interact with web page
+import requests
+#replace url with target url
+url = 'urlOfTarget'
+#user log in data
+data = {
+'User Id': 'username',
+'Password': 'Password',
+}
+#create element to call request and pass through given info, as text
+x=requests.post(url, data=data).text
+print(x.status_code)
